d9313365fba445d8a57154f786d7a69ca183a857
Manuel Tuschen <Manuel_Tuschen@web.de>
2016-06-25 16:30:06 +0200

    Updated Setup

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='fileHandler',
+setup(name='easyScripting',
       version='0.1',
-      description='Prepare for loading or saving files.',
-      url='https://github.com/manuSrep/fileHandlerPy.git',
-      author='Manuel_Tuschen',
+      description='easyScripting facilitates (scientific) python scripting. It does so by providing functionality for common tasks as saving and loading files.',
+      url='https://github.com/manuSrep/easyScriptingPy.git',
+      author='Manuel Tuschen',
       author_email='Manuel_Tuschen@web.de',
       license='FreeBSD License',
       packages=['fileHandler'],
